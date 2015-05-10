67c587dbde43a5ae5889ed44cce7e9e542906698
skaplun <shai.kaplun@gmail.com>
2015-05-10 08:41:04 +0000

    add jquery

---
+++ public/main.js
@@ -0,0 +1,14 @@
+require.config({
+  paths: {
+    'jquery': 'http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min',
+    'fabric': 'http://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.5.0/fabric.min',
+       
+  }
+
+});
+
+
+define(['jquery' ,'fabric'], function ($) {
+     fabric = window.fabric
+
+});
